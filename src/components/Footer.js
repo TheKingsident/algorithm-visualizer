58c155de9c4482479dff9b5ced979e206a4cf1b9
TheKingsident <kingsident@gmail.com>
2024-10-25 23:28:37 +0100

    Refactor Footer component to add GitHub link and remove redundant code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,15 +10,16 @@ function Footer() {
       </span>
 
       <div className="flex space-x-4 mt-4 md:mt-0">
+        <a href="https://github.com/TheKingsident" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-300">
+          <FaGithub size={20} />
+        </a>
         <a href="https://www.linkedin.com/in/thekingsident/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-300">
           <FaLinkedin size={20} />
         </a>
         <a href="https://x.com/CtrlAlt_Byte/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-300">
           <FaTwitter size={20} />
         </a>
-        <a href="https://github.com/TheKingsident" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-300">
-          <FaGithub size={20} />
-        </a>
+        
       </div>
     </div>
   </footer>
