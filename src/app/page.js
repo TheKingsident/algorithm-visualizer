efd76377700d9aca56743adb868b1eb72cf35248
TheKingsident <kingsident@gmail.com>
2024-10-28 01:35:47 +0100

    Refactor layout.js to remove unused code and move metadata to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,24 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Visualizer from '@/components/Visualizer';
 
+export const metadata = {
+  title: "Algorithm Visualizer",
+  description: "Explore, learn, and master sorting algorithms with our interactive Sorting Algorithm Visualizer. From classic algorithms like Heap Sort and Quick Sort to beginner-friendly Bubble Sort and Selection Sort, this tool allows you to watch each algorithm in action as it sorts an array in real time.",
+  openGraph: {
+      title: "Algorithm Visualizer",
+      description: "Explore, learn, and master sorting algorithms with our interactive Sorting Algorithm Visualizer. From classic algorithms like Heap Sort and Quick Sort to beginner-friendly Bubble Sort and Selection Sort, this tool allows you to watch each algorithm in action as it sorts an array in real time.",
+      url: "https://algorithm-visualizer-five-pi.vercel.app/",
+      images: [
+          {
+              url: "https://github.com/TheKingsident/algorithm-visualizer/blob/master/public/og-image.png",
+              width: 1200,
+              height: 630,
+              alt: "Hero Image",
+          },
+      ],
+  },
+};
+
 function App() {
   return (
     <div className='bg-gray-900 font-sans'>
