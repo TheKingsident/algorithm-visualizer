a6dedc8227076b263689e7b4e86751f766f38c78
TheKingsident <kingsident@gmail.com>
2024-10-28 01:27:55 +0100

    Add og-image for better social share previews

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter, Roboto, } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -18,6 +19,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Algorithm Visualizer" />
+        <meta property="og:description" content="Explore, learn, and master sorting algorithms with our interactive Sorting Algorithm Visualizer. From classic algorithms like Heap Sort and Quick Sort to beginner-friendly Bubble Sort and Selection Sort, this tool allows you to watch each algorithm in action as it sorts an array in real time."/>
+        <meta property="og:image" content="https://yourdomain.com/path-to-your-image.png" />
+      </Head>
       <body
         className={inter.className}
       >
